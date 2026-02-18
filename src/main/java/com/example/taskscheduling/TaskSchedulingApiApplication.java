24d410853f7ce568839f8eb64d12e6bfb3c412a6
Ritika Raj <rajritika999@gmail.com>
2026-02-18 23:05:18 +0530

    Initial commit for Project

---
+++ src/main/java/com/example/taskscheduling/TaskSchedulingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskscheduling/TaskSchedulingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
