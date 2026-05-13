d1013ccba4cbdfe20e51eec3e7ff194955047062
Ritika Raj <rajritika999@gmail.com>
2026-05-13 17:12:37 +0530

    Initial Commit for Task Scheduling Api Project

---
+++ task-scheduling-api/src/test/java/com/taskscheduling/api/TaskSchedulingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskscheduling.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
